fba2e5157657fbe56ee163b2e02d6e3549195d86
abobaker sdeeg <abobaker319@gmail.com>
2025-02-17 14:13:45 +0300

    init

---
+++ Classroom.API/Domain/Entities/User.cs
@@ -0,0 +1,20 @@
+﻿namespace Classroom.API.Domain.Entities
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
+        public string? Phone { get; set; } 
+        public int RoleId { get; set; }
+        public Role? Role { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+        public List<UserClassroom> UserClassrooms { get; set; } = new List<UserClassroom>();
+        public List<UserQuizAttempt> QuizAttempts { get; set; } = new List<UserQuizAttempt>();
+        public List<UserRole> UserRoles { get; set; } = new List<UserRole>();
+
+
+    }
+}
